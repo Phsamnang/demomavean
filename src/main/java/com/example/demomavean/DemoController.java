6165b1a3db786e90a6dc71c0be804ce424fb90a3
KSGA-Autopilot <kshrdautopilot@gmail.com>
2023-11-08 16:24:50 +0700

    updated

---
+++ src/main/java/com/example/demomavean/DemoController.java
@@ -0,0 +1,12 @@
+package com.example.demomavean;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+    @GetMapping("/hello")
+    public String test(){
+        return "hello test";
+    }
+}
