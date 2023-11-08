70df3bbc2b87970331a2e735f2e030341b2995d1
KSGA-Autopilot <kshrdautopilot@gmail.com>
2023-11-08 16:23:47 +0700

    updated

---
+++ src/main/java/com/example/demomavean/DemoMaveanApplication.java
@@ -0,0 +1,13 @@
+package com.example.demomavean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMaveanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoMaveanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demomavean/DemoMaveanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demomavean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMaveanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
